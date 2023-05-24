7bc86532a10af05c0c8857a64541933ceb76d17a
seth <89246806+missing-body@users.noreply.github.com>
2023-05-24 22:37:23 +1000

    Create spraycan-crash.lua

---
+++ Da Hood/spraycan-crash.lua
@@ -0,0 +1,35 @@
+getgenv().SprayCans = 0;
+getgenv().SelectedGroup = nil;
+
+local Groups = game:GetService("GroupService"):GetGroupsAsync(game:GetService("Players").LocalPlayer.UserId);
+
+for i, v in next, Groups do
+    if v == 1 then
+        SelectedGroup = v;
+    end;
+end;
+
+task.spawn(function()
+    while task.wait() do
+        for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+            if v.Name == "[SprayCan]" then
+                v.Parent = game:GetService("Players").LocalPlayer.Character;
+            end;
+        end;
+    end;
+end);
+
+task.spawn(function()
+    while task.wait() do
+        game:GetService("ReplicatedStorage").MainEvent:FireServer("JoinCrew", SelectedGroup);
+        game:GetService("ReplicatedStorage").MainEvent:FireServer("LeaveCrew");
+        SprayCans = SprayCans + 1;
+        if SprayCans >= 3000 then
+            for i, v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
+                if v:IsA("BasePart") then
+                    v:Destroy();
+                end;
+            end;
+        end;
+    end;
+end);
