74a31fa87049ed4d968bfb37c28cc7aa1c7841bd
seth <89246806+missing-body@users.noreply.github.com>
2023-06-03 21:07:24 +1000

    Update walkspeed-bypass.lua

--- Universal/walkspeed-bypass.lua
+++ Universal/walkspeed-bypass.lua
@@ -1,9 +1,9 @@
-local old_hookmetamethod;
-old_hookmetamethod = hookmetamethod(game, "__index", function(Self, Key)
+local old;
+old = hookmetamethod(game, "__index", function(Self, Key)
     if Key == "Walkspeed" and Self == game:GetService("Players").LocalPlayer.Character.Humanoid or Key == "JumpPower" and Self == game:GetService("Players").LocalPlayer.Character.Humanoid then
         return nil;
     end;
-    return old_hookmetamethod(Self, Key);
+    return old(Self, Key);
 end);
 
 game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = 100;
